6958379976b06c474cb77b7cd83ea17a596d1ba2
Ethan Johnson <ejohnson3196@stu.d214.org>
2021-10-01 14:43:04 -0500

    adding code

--- Commsionn/ViewController.swift
+++ Commsionn/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var BasePayLabel: UIStackView!
+    @IBOutlet weak var CommisionPayLabel: UITextField!
+    @IBOutlet weak var TotalPayLabel: UILabel!
+    
+    
+    let basePay = 500
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func WhenButtonPressed(_ sender: Any) {
+    }
+    
 
 }
 
